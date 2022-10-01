12c03041693e902370ea7aaaecda32d38efc67a1
vportens <viporten@student.42.fr>
2022-10-01 21:20:35 +0200

    merge fix

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { ConfigService } from '@nestjs/config';
+import { UserService } from '../users/users.service';
 import { JwtService } from "@nestjs/jwt";
 import { MailService } from '../mail/mail.service';
 
@@ -10,12 +11,14 @@ export class AuthService {
 		private prisma: PrismaService,
 		private conf: ConfigService,
 		private jwt: JwtService,
+		private userService: UserService,
 		private mailService: MailService ) {}
+
 
 	async signin( login: string ) {
 		let user = await this.prisma.user.findFirst({ where: { login: login }});
 		if (!user)
-		user = await this.prisma.user.create({
+			user = await this.prisma.user.create({
 			data: {
 				login: login,
 				email: login + '@student.42.fr',
@@ -32,7 +35,7 @@ export class AuthService {
 		return token;
 	}
 
-	async signToken( userId: number, login: string ): Promise<{access_token: string}> { 
+	async signToken( userId: number, login: string ): Promise<{ access_token: string}> { 
 		const payload = { sub: userId, login }
 		const secret = this.conf.get('JWT_SECRET');
 		const token = await this.jwt.signAsync(payload, {expiresIn: '30m', secret: secret});
