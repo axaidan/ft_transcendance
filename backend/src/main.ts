c6ed7b89cc2789f0e2458473c799e4689b65d616
Maxime LORMOIS <mlormois@e1r5p8.clusters.42paris.fr>
2022-10-06 19:50:06 +0200

    init de ma branche

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,19 +4,22 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule);
 
 	app.use(cookieParser());
 	// app.use( function(req, res, next) {
 	// 	res.header("Access-Control-Allow-Origin", "http://localhost:4200");
 	// 	// res.header("Access-Control-Allow-Credentials", "true");
 	// 	res.header("Access-Control-Allow-Headers", "Authorization,  Origin, Content-Type");
-		
+
 	// 	next();
 	// });
 	app.enableCors();
+
+
+
 	app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  	await app.listen(3000);
+	await app.listen(3000);
 }
 
 bootstrap();
