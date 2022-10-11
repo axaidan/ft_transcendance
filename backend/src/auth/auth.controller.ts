1c51b4a78b5da1f4a9284f074d041ca5f4cb58bb
vportens <mlormois@student.42.fr>
2022-10-11 14:28:50 +0200

    fake-users - u can sign in at: 4200/signinTest

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -7,28 +7,28 @@ import { SigninTestDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
-	constructor(private authService: AuthService) {}
+	constructor(private authService: AuthService) { }
 
 	@UseGuards(FtGuard)
 	@HttpCode(HttpStatus.OK)
 	@Get('signin')
-	signin( @Req() req: Request) { /* NOTHING TO DO HERE, I DONT KNOW */ }
-	
+	signin(@Req() req: Request) { /* NOTHING TO DO HERE, I DONT KNOW */ }
+
 	@UseGuards(FtGuard)
 	@Get('callback')
-	async callback( @Req() req: Request, @Res({ passthrough: true }) response: Response ) {
+	async callback(@Req() req: Request, @Res({ passthrough: true }) response: Response) {
 		let token: string;
-		await this.authService.signin( req.user.toString() )
-		.then( (res) => {
-			token = res;
-			response.cookie('access_token', token, {
-				expires: new Date(Date.now() + 6000000)
-			});
-			response.redirect("http://localhost:4200/home");
-		})
-		.catch( (e) => {
-			response.redirect("http://localhost:4200");
-		})
+		await this.authService.signin(req.user.toString())
+			.then((res) => {
+				token = res;
+				response.cookie('access_token', token, {
+					expires: new Date(Date.now() + 6000000)
+				});
+				response.redirect("http://localhost:4200/home");
+			})
+			.catch((e) => {
+				response.redirect("http://localhost:4200");
+			})
 	}
 
 	@Get('2fa-callback')
@@ -41,22 +41,24 @@ export class AuthController {
 		response.redirect("http://localhost:4200/home");
 	}
 
-	@Post('signin-test')
+	@Get('signin-test')
 	async signinTest(
-		@Body() dto: SigninTestDto,
+		@Query('login') login : string,
 		@Res({ passthrough: true }) response: Response
-		) {
+	) {
 		let token: string;
-		await this.authService.signinTest( dto.login )
-		.then( (res) => {
-			token = res;
-			response.cookie('access_token', token, {
-				expires: new Date(Date.now() + 6000000)
-			});
-			response.redirect("http://localhost:4200/home");
-		})
-		.catch( (e) => {
-			response.redirect("http://localhost:4200");
-		})
+		console.log("AuthController - signinTest(" + login + ")");
+		await this.authService.signinTest(login)
+			.then((res) => {
+				token = res;
+				response.cookie('access_token', token, {
+					expires: new Date(Date.now() + 6000000)
+				});
+				console.log("AuthController - redirecting to 4200/home");
+				response.redirect("http://localhost:4200/home");
+			})
+			.catch((e) => {
+				response.redirect("http://localhost:4200/signinTes");
+			})
 	}
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -11,55 +11,58 @@ export class AuthService {
 		private prisma: PrismaService,
 		private conf: ConfigService,
 		private jwt: JwtService,
-		private mailService: MailService ) {}
+		private mailService: MailService) { }
 
 
-	async signin( login: string ) {
-		let user = await this.prisma.user.findFirst({ where: { login: login }});
+	async signin(login: string) {
+		let user = await this.prisma.user.findFirst({ where: { login: login } });
 		if (!user)
 			user = await this.prisma.user.create({
-			data: {
-				login: login,
-				username: login,
-				email: login + '@student.42.fr',
-			}
-		});
+				data: {
+					login: login,
+					username: login,
+					email: login + '@student.42.fr',
+				}
+			});
 
-		const token = await this.signToken( user.id, user.login ); 
+		const token = await this.signToken(user.id, user.login);
 		if (user.twoFactorAuth === true) {
 			this.mailService.sendLoginToken(user, token.access_token);
 			return undefined;	// SHOULD RETURN INFO TO MAKE A
-								// "CHECK YOUR MAIL TO LOGIN"
-								// WINDOW POP-UP
+			// "CHECK YOUR MAIL TO LOGIN"
+			// WINDOW POP-UP
 		}
 
 		// console.log(token.access_token);
-		return token.access_token ;
+		return token.access_token;
 
 	}
 
-	async signToken( userId: number, login: string ): Promise<{ access_token: string}> { 
+	async signToken(userId: number, login: string): Promise<{ access_token: string }> {
 		const payload = { sub: userId, login }
 		const secret = this.conf.get('JWT_SECRET');
-		const token = await this.jwt.signAsync(payload, {expiresIn: '30m', secret: secret});
+		const token = await this.jwt.signAsync(payload, { expiresIn: '30m', secret: secret });
 		return { access_token: token };
 	}
 
 
 	//	FAKE-USER-BEGIN !!!
-	async signinTest( login: string ) {
-		let user = await this.prisma.user.findFirst({ where: { login: login }});
+	async signinTest(login: string) {
+		console.log("AuthService - signinTest(" + login + ")");
+		let user = await this.prisma.user.findFirst({ where: { login: login } });
 		if (!user) {
+			console.log("AuthService - " + login + " NOT FOUND");
 			throw new NotFoundException(`TEST SIGNIN FAILED - USER ${login} NOT FOUND`);
 		};
-		const token = await this.signToken( user.id, user.login ); 
+		const token = await this.signToken(user.id, user.login);
 		if (user.twoFactorAuth === true) {
 			this.mailService.sendLoginToken(user, token.access_token);
 			return undefined;	// SHOULD RETURN INFO TO MAKE A
-								// "CHECK YOUR MAIL TO LOGIN"
-								// WINDOW POP-UP
+			// "CHECK YOUR MAIL TO LOGIN"
+			// WINDOW POP-UP
 		}
-		return token.access_token ;
+		console.log("AuthService - " + login + " FOUND");
+		return token.access_token;
 	}
 	//	FAKE-USER-END !!!
 }
