e0e3ff0b4428918867f29916e44d53967c688320
WalterLuong <wluong@student.42.fr>
2022-10-25 21:45:15 +0200

    pulling

--- backend/src/discussion-message/dto/discussion-message.dto.ts
+++ backend/src/discussion-message/dto/discussion-message.dto.ts
@@ -1,15 +1,15 @@
 import { IsInt, IsNotEmpty, IsString } from 'class-validator';
 
 export class DiscussionMessageDto {
-   
-    @IsInt()
-    userId: number;
 
-    @IsInt()
-    discId: number;
+	@IsInt()
+	userId: number;
 
-    @IsString()
-    @IsNotEmpty()
-    text: string;
-    
+	@IsInt()
+	discId: number;
+
+	@IsString()
+	@IsNotEmpty()
+	text: string;
+
 }
